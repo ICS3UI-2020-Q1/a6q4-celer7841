f2882268587d2dca97bbf09e54c9397e94cd8517
celer7841 <celer7841@wrdsb.ca>
2020-10-13 02:21:46 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author celer7841
  */
 public class Main {
 
@@ -10,6 +11,26 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    int[] firstVector = new int[3];
+    int[] secondVector = new int[3];
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter the 3 values for the first vector");
+    
+   firstVector[0] =  sc.nextInt();
+   firstVector[1] =  sc.nextInt();
+   firstVector[2] =  sc.nextInt();
+
+   System.out.println("Please enter the 3 values for the second vector");
+    
+   secondVector[0] =  sc.nextInt();
+   secondVector[1] =  sc.nextInt();
+   secondVector[2] =  sc.nextInt();
+
+   int result = firstVector[0] * secondVector[0] + firstVector[1] * secondVector[1] + firstVector[2] * secondVector[2];
+
+
+   System.out.println("The dot product is " + result);
+    
     
   }
 }
